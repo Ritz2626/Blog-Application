fd514ced9867b9bf755839f6145853e6b7e3b6c9
Ritika2626 <ritikarastogi2626@gmail.com>
2022-11-12 22:06:29 +0530

    Added styling and further functionalities

--- blog/post/models.py
+++ blog/post/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+
 # Create your models here.
 class Post(models.Model):
     title=models.CharField(max_length=100)
+    
     author=models.ForeignKey(User,on_delete=models.CASCADE)
-    description=models.TextField()
-
+    description=HTMLField()
+    date1=models.DateField(null=True)
+    cover_photo=models.ImageField(null=True, blank=True ,upload_to = 'cover')
+   
+   
     def __str__(self):
         return self.title +"|"+str(self.author)
         
--- blog/post/views.py
+++ blog/post/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import *
 # Create your views here.
 def index(request):
-    store=Post.objects.all()
+    store=Post.objects.order_by('-date1')
     print(store)
     return render(request,'index.html',{'store':store})
 
